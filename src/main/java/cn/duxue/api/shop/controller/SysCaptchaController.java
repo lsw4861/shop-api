12775e07c5643a15c03fc46310e2482bef766c27
lsw4861 <294880292@qq.com>
2019-07-18 17:52:15 +0800

    代码调整

--- src/main/java/cn/duxue/api/shop/controller/SysCaptchaController.java
+++ src/main/java/cn/duxue/api/shop/controller/SysCaptchaController.java
@@ -5,6 +5,7 @@ import cn.duxue.api.shop.service.SysCaptchaService;
 import cn.duxue.common.utils.PageUtils;
 import cn.duxue.common.utils.ResultUtils;
 import cn.duxue.common.vo.ResultVo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,20 @@ public class SysCaptchaController {
     public ResultVo list(@RequestBody SysCaptchaEntity sysCaptchaEntity){
         PageUtils pageUtils = sysCaptchaService.queryPage(sysCaptchaEntity);
         return ResultUtils.success(pageUtils);
+    }
+
+    @ResponseBody
+    @PostMapping("/add")
+    public ResultVo add(@RequestBody SysCaptchaEntity sysCaptchaEntity){
+        boolean save = sysCaptchaService.save(sysCaptchaEntity);
+        return ResultUtils.success(save);
+    }
+
+    @ResponseBody
+    @GetMapping("/edit/{id}")
+    public ResultVo edit(@PathVariable("id") long id){
+        SysCaptchaEntity sysCaptchaEntity = sysCaptchaService.getById(id);
+        return ResultUtils.success(sysCaptchaEntity);
     }
 
 
--- src/main/java/cn/duxue/api/shop/entity/SysCaptchaEntity.java
+++ src/main/java/cn/duxue/api/shop/entity/SysCaptchaEntity.java
@@ -2,7 +2,9 @@ package cn.duxue.api.shop.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -31,6 +33,8 @@ public class SysCaptchaEntity extends BaseEntity {
 	/**
 	 * 过期时间
 	 */
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
 	private Date expireTime;
 
 }
